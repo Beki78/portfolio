38c27eec68a0b3da38bd103f21703d4913296a06
Bereket Getachew <146061009+Beki78@users.noreply.github.com>
2023-12-17 13:08:22 +0300

    Update script.js
    removing the contact form code from js

--- script.js
+++ script.js
@@ -5,14 +5,3 @@ function scrollToContact() {
   });
 }
 
-function sendMail() {
-  Email.send({
-    Host: "smtp.gmail.com",
-    Username: "bakiget78@gmail.com",
-    Password: "0978101611",
-    To: "bakiget78@gmail.com",
-    From: document.getElementById("email").value,
-    Subject: "New Contact Form",
-    Body: "And this is the body",
-  }).then((message) => alert(message));
-}
